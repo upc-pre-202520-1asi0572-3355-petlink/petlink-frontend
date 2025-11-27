220612acc85e05ad3c458bd231f7ebadf8a408be
Developer <danielj.chavarriz@gmail.com>
2025-11-26 21:47:44 -0500

    feat(petlink): update users

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environment.prod.js';
 
 interface Usuario {
   id: number;
